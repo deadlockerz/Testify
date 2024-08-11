dcee1bab34fd6acac955d729a05ddb6e7dfaf1bc
GOURAV VISHWAKARMA <gouravvishwakarma049@gmail.com>
2024-08-11 10:18:51 +0530

    Login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="658716736046-lrhfa8d733m376vdf0n8mqerpoct6o1v.apps.googleusercontent.com">
+   
       <AppWrapper />  {/* Use AppWrapper directly here */}
-    </GoogleOAuthProvider>
+    
   </React.StrictMode>
 );
--- frontend/src/router/Router.js
+++ frontend/src/router/Router.js
@@ -1,71 +1,3 @@
-// // Import statements
-// import Hero from "../components/Hero";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import LoginForm from "../pages/Login";
-// import SignupForm from "../pages/Signup";
-// import Courses from "../pages/Courses";
-// import Practice from "../pages/Practice";
-// import Cart from "../pages/Cart";
-// import NotFound from "../components/NotFound";
-// import Error from "../components/Error";
-// import AdminHome from "../admin/AdminHome";
-// import AdminPractice from "../admin/AdminPractice";
-// import UpdateCourse from "../admin/UpdateCourse";
-// import CardPayment from "../pages/CardPayment";
-// import UpiPayment from "../pages/UpiPayment";
-// import BankPayment from "../pages/BankPayment";
-// import PayPalPayment from "../pages/PayPalPayment";
-// import CourseDetail from "../pages/CourseDetail";
-// import ForgotPassword from "../pages/ForgotPassword";
-// import ResetPassword from "../pages/ResetPassword";
-// import { GoogleOAuthProvider } from '@react-oauth/google';
-// import App from "../App";
-
-// // Router component
-// const Router = () => {
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <App />, 
-//       errorElement: <Error />,
-//       children: [
-//         { path: "/", element: <Hero /> },
-//         { path: "/login", element: <LoginForm /> },
-//         { path: "/signup", element: <SignupForm /> },
-//         { path: "/courses", element: <Courses /> },
-//         { path: "/practice", element: <Practice /> },
-//         { path: "/cart", element: <Cart /> },
-//         { path: "/admin", element: <AdminHome /> },
-//         { path: "/adminpractice", element: <AdminPractice /> },
-//         { path: "/updatecourse/:id", element: <UpdateCourse /> },
-//         { path: "/credit-card-payment", element: <CardPayment /> },
-//         { path: "/upi-payment", element: <UpiPayment /> },
-//         { path: "/bank-transfer-payment", element: <BankPayment /> },
-//         { path: "/paypal-payment", element: <PayPalPayment /> },
-//         { path: "/course/:id", element: <CourseDetail /> },
-//         { path: "/forgot-password", element: <ForgotPassword /> },
-//         { path: "/reset-password/:id/:token", element: <ResetPassword /> },
-//         { path: "*", element: <NotFound /> },
-//       ],
-//     },
-//   ]);
-
-//   return <RouterProvider router={router} />;
-// };
-
-// // Wrapping the app with GoogleOAuthProvider
-// const AppWrapper = () => {
-//   return (
-//     <GoogleOAuthProvider clientId="658716736046-lrhfa8d733m376vdf0n8mqerpoct6o1v.apps.googleusercontent.com"> 
-//           <Router/>
-//     </GoogleOAuthProvider>
-//   );
-// };
-
-// export default AppWrapper; 
-
-
-//Components
 import Hero from "../components/Hero";
 import { createBrowserRouter , RouterProvider } from "react-router-dom";
 import LoginForm from "../pages/Login";
@@ -136,9 +68,8 @@ import App from "../App";
   ]);
   const AppWrapper = () => {
     return (
-      <GoogleOAuthProvider clientId="658716736046-lrhfa8d733m376vdf0n8mqerpoct6o1v.apps.googleusercontent.com">
+     
         <RouterProvider router={router} />
-      </GoogleOAuthProvider>
     );
   };
   
